d36af92b89bd67c2e0772601e58c624adc3ad416
abhishek <abhishekanandjha48@gmail.com>
2023-01-13 19:32:49 +0530

    Level 4 - Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-//level 3 encryption///hashing///
-const md5=require("md5");
+const bcrypt =require("bcrypt");//level 4 encryption//
+const saltRounds = 10;//level 4 encryption//
+//const md5=require("md5");//level 3 encryption///hashing///
 //const encrypt = require('mongoose-encryption');//level 2
 
 const app = express();
@@ -44,30 +45,36 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUser= new User({
-    email:req.body.username,
-    password:md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser= new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=md5(req.body.password);
+  const password=req.body.password;
   User.findOne({email:username},function(err,foundUser){
       if(err){
         console.log(err);
       }else{
         if(foundUser){
-          if(foundUser.password===password){
-          res.render("secrets");
-          }
+          bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result===true){
+              res.render("secrets");
+            }
+          });
         }
       }
   });
