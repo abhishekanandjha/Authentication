f7d1f36eef98a77b140865d47ba3dbd111058f65
abhishek <abhishekanandjha48@gmail.com>
2023-01-13 12:34:06 +0530

    Add evvironment vars

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -16,12 +16,13 @@ app.use(express.static("public"));
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://0.0.0.0:27017/userDB", { useNewUrlParser: true });
 
+///this is documentation for mongoose encryption//////////https://www.npmjs.com/package/mongoose-encryption?activeTab=readme////////////////////
 const userSchema=new mongoose.Schema( {
   email:String,
   password:String
 });
-const secret="Thisisourlittlesecret.";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 const User= new mongoose.model("User",userSchema);
 
 
