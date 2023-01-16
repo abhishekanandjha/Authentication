5d9744c8e5238d8e3e8c81b867bd27e608faaa4d
abhishek <abhishekanandjha48@gmail.com>
2023-01-16 16:13:15 +0530

    Level 6 - Google OAuth 2.0 Authentication

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport=require("passport");
 const passportlocalmongoose =require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
+
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 mongoose.set("strictQuery", false);
 
 app.use(session({
@@ -27,28 +30,128 @@ mongoose.connect("mongodb://0.0.0.0:27017/userDB", { useNewUrlParser: true });
 
 const userSchema=new mongoose.Schema( {
   email:String,
-  password:String
+  password:String,
+  googleId:String,
+  secret:String
 });
 
 userSchema.plugin(passportlocalmongoose);
+userSchema.plugin(findOrCreate);
+
 
 
 const User= new mongoose.model("User",userSchema);
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+//check
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+      picture: user.picture
+    });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
+//
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function(err, user) {
+//     done(err, user);
+//   });
+// });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    //console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //TODO
 app.get("/",function(req,res){
   res.render("home");
 });
+//down will bring google popup for register or signin
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })
+);
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+
 app.get("/login",function(req,res){
   res.render("login");
 });
 app.get("/register",function(req,res){
   res.render("register");
 });
+
+app.get("/secrets",function(req,res){
+
+  User.find({"secret":{$ne: null}},function(err,founduser){
+    if(err){
+      console.log(err);
+    }else{
+      if(founduser){
+        res.render("secrets",{userwithsecrets:founduser});
+      }
+    }
+  });
+  // if(req.isAuthenticated()){
+  //   res.render("secrets");
+  // }else{
+  //   res.redirect("/login");
+  // }
+});
+
+app.get("/submit",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit",function(req,res){
+  const submittedSecret=req.body.secret;
+
+  User.findById(req.user.id, function(err,founduser){
+    if(err){
+      console.log(err);
+    }else{
+      if(founduser){
+        founduser.secret=submittedSecret;
+        founduser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
 app.get("/logout",function(req,res){
   req.logout(function(err){
     if(err){
@@ -57,15 +160,6 @@ app.get("/logout",function(req,res){
       res.redirect("/");
     }
   });
-
-});
-
-app.get("/secrets",function(req,res){
-  if(req.isAuthenticated()){
-    res.render("secrets");
-  }else{
-    res.redirect("/login");
-  }
 });
 
 app.post("/register",function(req,res){
